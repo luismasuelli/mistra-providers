e3b9bbc3bfcefd0090a91d9ab54c37c8bc11f10a
Luis Masuelli <luisfmasuelli@gmail.com>
2024-11-29 16:57:58 -0500

    Also pushing the version of mistra-providers.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mistra-providers',
-    version='0.0.7',
+    version='0.1.0',
     packages=['mistra.providers.historical.preprocessing',
               'mistra.providers.historical.raw.filesystem',
               'mistra.providers.historical.raw.truefx'],
@@ -11,6 +11,6 @@ setup(
     author='luismasuelli',
     author_email='luisfmasuelli@gmail.com',
     description='MISTRA Providers contains a collection of custom providers for the MISTRA package',
-    python_requires='>=3.3',
-    install_requires=['mistra>=0.0.10', 'pandas==0.25.3']
+    python_requires='>=3.12',
+    install_requires=['mistra>=0.1.0', 'pandas>=2.2.3']
 )
